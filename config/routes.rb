f409e6ab1fe24a41c7c8d27a32a652d95b83e02d
Joysi Soza <sozajoysi14@gmail.com>
2024-10-09 15:50:27 -0600

    guia9 apartir de la guia8

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
+  # Rutas para las páginas estáticas
   get "static_pages/home"
-  get "static_pages/help"
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Comprobación de estado
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/*
+  # Archivos PWA
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  #root "posts#index"
-
+  # Define la ruta raíz
   root 'static_pages#home'
 end
